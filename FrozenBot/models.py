f4f618f4eef72c6f1decf764de5963b55f766a76
armycrih <armycrih@gmail.com>
2022-11-06 15:22:22 -0300

    Lat, Lon Pehuajo

--- FrozenBot/models.py
+++ FrozenBot/models.py
@@ -4,8 +4,8 @@ import math
 class GeoApi():
     def __init__(self):
         self.API_KEY = "17d7346c70e83abde4eceb82b84f8c74"#"d81015613923e3e435231f2740d5610b"
-        self.LAT = "-24.7859000"#"-35.836948753554054"
-        self.LON = "-65.4116600"#"-61.870523905384076"
+        self.LAT = "-35.836948753554054"#"-24.7859000" Pehuajo / Salta respectivamente
+        self.LON = "-61.870523905384076"#"-65.4116600" Pehuajo / Salta respectivamente
        
         parameters = {"lat": self.LAT, "lon": self.LON, "appid": self.API_KEY}
         self.temperature = None
