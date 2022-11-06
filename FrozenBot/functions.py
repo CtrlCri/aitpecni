94de224e4e1814a10a05982d91166989735c6264
armycrih <armycrih@gmail.com>
2022-11-06 16:22:04 -0300

    def is_product_available and format_products

--- FrozenBot/functions.py
+++ FrozenBot/functions.py
@@ -1,1 +1,20 @@
-import pandas as pd
+def format_products(_PRODUCT_DF: list):
+    """ 
+    Remove out of stock products and 
+    Assigning a new column for product codes 
+    """
+    products = _PRODUCT_DF[_PRODUCT_DF["quantity"]>0] # Only products in stock
+    count = len(products.index) # list length
+    codes = []
+    for i in range(1, count+1):
+        codes.append(i)
+    products = products.assign(code=codes) # Assignment here
+
+    return products
+
+def is_product_available(codes: list, code: int):
+    for item in codes:
+        if code == item:
+            return True
+     
+    return False
--- FrozenBot/index.py
+++ FrozenBot/index.py
@@ -1,7 +1,14 @@
 from models import GeoApi
+from functions import format_products, is_product_available
 
-weather = GeoApi()
+import pandas as pd
 
+_PRODUCT_DF = pd.DataFrame({"product_name": ["Chocolate", "Granizado",
+"Limon", "Dulce de Leche"], "quantity": [3,10,0,5]})
+
+products = format_products(_PRODUCT_DF)
+
+weather = GeoApi() # Class instance
 
 if __name__ == "__main__":
     
@@ -10,3 +17,16 @@ if __name__ == "__main__":
     else:
         print("Welcome! Nice weather, the temparature is : " + str(weather.get_temperature()))
     
+    print("Ice Cream Flavor")
+    print(products)
+    while True:
+        try:
+            print("Please enter a ice cream flavor. Entry correct code, for example")
+            sample = products.sample()
+            print(f' for {sample["product_name"].unique()} entry code: {sample["code"].unique()}')
+            code = int(input("Entry code : "))
+            if is_product_available(products["code"], code):
+                break
+            else: print("Oops!  No available.  Try again...")
+        except ValueError:
+            print("Oops!  That was no valid code.  Try again...")
